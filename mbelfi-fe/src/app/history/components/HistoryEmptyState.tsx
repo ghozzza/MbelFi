d86184b42cdc237e77c30d15b0c349aca5c73d15
wildanre <mws02505@gmail.com>
2025-08-04 00:23:47 +0700

    Remove unnecessary background blur from HistoryEmptyState component

--- mbelfi-fe/src/app/history/components/HistoryEmptyState.tsx
+++ mbelfi-fe/src/app/history/components/HistoryEmptyState.tsx
@@ -5,7 +5,6 @@ const HistoryEmptyState: React.FC = () => {
   return (
     <div className="text-center py-12">
       <div className="relative mb-4">
-        <div className="absolute inset-0 bg-gradient-to-r from-gray-500/20 to-gray-400/20 rounded-full blur-sm"></div>
         <div className="relative bg-gradient-to-br from-gray-800 to-gray-700 p-4 rounded-full border border-gray-600/50 mx-auto w-16 h-16 flex items-center justify-center">
           <Clock className="w-8 h-8 text-gray-400" />
         </div>
